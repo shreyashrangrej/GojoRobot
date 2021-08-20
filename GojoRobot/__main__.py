3904fe55183eaa27a59d20d913db22ace8e2df37
Shreyash Rangrej <45220271+shreyashrangrej@users.noreply.github.com>
2021-08-20 16:16:50 +0530

    Update __main__.py

--- GojoRobot/__main__.py
+++ GojoRobot/__main__.py
@@ -189,7 +189,7 @@ def start(update: Update, context: CallbackContext):
                     update.effective_chat.id,
                     HELPABLE[mod].__help__,
                     InlineKeyboardMarkup(
-                        [[InlineKeyboardButton(text="⬅️ BACK", callback_data="start")]]
+                        [[InlineKeyboardButton(text="⬅️ BACK", callback_data="start_back")]]
                     ),
                 )
 
